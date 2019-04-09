712e7b8f16269c04ec933c632f35abf7e8011863
Le <hu.le.ca@gmail.com>
2019-04-09 19:02:09 -0400

    updated ...

--- app/src/main/java/Fragments/ChatsFragment.java
+++ app/src/main/java/Fragments/ChatsFragment.java
@@ -1,16 +1,13 @@
 package Fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.lehu.mychat.R;
+
+import androidx.fragment.app.Fragment;
 
 
 public class ChatsFragment extends Fragment {
